902bc0c1231817f72bd789b661b2e1f382a83601
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-22 21:48:09 +0900

    24.12.22 : Bean 조회

--- core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -1,17 +1,21 @@
 package hello.core.beanfind;
 
-import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.MemberServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class ApplicationContextExtendsFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
@@ -32,7 +36,27 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("특정 하위 타입으로 조회")
     void findBeanBySubType() {
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
 
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+        for (String s : beansOfType.keySet()) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 -Object")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String s : beansOfType.keySet()) {
+            System.out.println("s = " + s);
+        }
     }
 
     @Configuration
